305360e4aa4402e8547c0c61583815c4ecf43195
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-11-27 11:08:29 +0530

    Add files via upload

---
+++ VariablesandConstants_DataTypes/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+//    variable: vary or change
+//    variable: container: it store any type of data
+
+    const int a = 10; // declaration/Creation and initialization/Assignment
+
+    printf("A is: %d\n", a); // \n- escape character, new line or enter
+
+    //constant: fixed value
+
+    //a = 50; // error, as a is constant now.
+
+    printf("A is: %d", a);
+
+    return 0;
+}
